41bcbd3e6ecaa3ea6df6e60abd3ce6952efa5421
Al Bondad <albondad@gmail.com>
2021-01-18 21:57:36 -0600

    Update express server and start script

--- application.js
+++ application.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const path = require('path');
 
 const application = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 application.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+
+application.get('/bundle.js', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/bundle.js'))
 });
 
 application.listen(port, () => {
