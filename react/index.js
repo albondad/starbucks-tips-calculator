c9e7298cf96ab1ef6ffe443e17b52ef18017d559
Al Bondad <albondad@gmail.com>
2021-01-28 22:41:03 -0600

    Refactor for use next.js

--- react/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Application from './components/application'
-
-const containerElement = document.querySelector("#root");
-
-ReactDOM.render(<Application />, containerElement)
