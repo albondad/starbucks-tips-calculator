16052aad534a07b8e893ac7e438e9b3c85819b6a
Al Bondad <albondad@gmail.com>
2021-02-07 11:38:07 -0600

    Update icon component

--- components/shared/icon/index.js
+++ components/shared/icon/index.js
@@ -8,7 +8,7 @@ export const Icon = (props) => {
     }
 
     return (
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg width={props.size} height={props.size} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path 
                 d={getPath(props.name)} 
                 fill={props.color}
